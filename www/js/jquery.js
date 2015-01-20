af7e4b86b6fbd066b01ea067eef8a791ec6a7c7d
aniellepizzo <aniellepizzo@gmail.com>
2015-01-20 14:31:38 -0200

    Terceiro APK

--- www/js/jquery.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() { 
-	$("#btn-concluir").click(function() {
-		$("#row-medicamento").fadeOut();
-	});
-});
---
+++ www/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() { 
+
+	$("#list-remedios").hide();
+	$("#list-medicamentos").hide();
+	$("#btns-medicamentos").hide();
+
+	$("#add-remedio").click(function() {
+		$(this).hide();
+		$("#list-remedios").hide();
+		$("#list-medicamentos, #btns-medicamentos").fadeIn();
+	});
+	
+	$("#btn-concluir").click(function() {
+		$("#list-medicamentos, #btns-medicamentos").hide();
+		$("#list-remedios, #add-remedio").fadeIn();
+	});
+});
